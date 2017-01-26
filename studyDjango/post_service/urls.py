1ef4c77b21204fb48577532f446cf50b54370e21
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-01-27 00:01:50 +0900

    [Django Rest Framework] make Board #3 – 간단한 Login 만들기 / django form 클래스를 활용하여 폼 만들기 @ 폼으로 데이터 전달이 안됨....ㅜㅜㅜ

--- studyDjango/post_service/urls.py
+++ studyDjango/post_service/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, include, url
 from post_service.views import *
 
 urlpatterns = patterns('',
-    url(r'^$', post_list)
+    url(r'^$', post_list),
+    url(r'^login/$', login),
+    url(r'^login/validate', login_validate),
 )
--- studyDjango/post_service/views.py
+++ studyDjango/post_service/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.contrib import auth
+from django.shortcuts import render, redirect
 from django.template import Context
+from django.template.context_processors import csrf
 from django.template.loader import get_template
 from django.http.response import HttpResponse
+
+from post_service.forms import LoginForm
 from post_service.models import Post
 from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
@@ -15,6 +19,9 @@ def post_list(request):
 
     page_data = Paginator(Post.objects.all(), 5)
     page = request.GET.get('page')
+
+    if page is None:
+        page = 1
     try:
         posts = page_data.page(page)
     except PageNotAnInteger:
@@ -22,6 +29,31 @@ def post_list(request):
     except EmptyPage:
         posts = page_data.page(page_data.num_pages)
 
-    context = Context({'post_list' : posts, 'current_page' : page, 'total_page': range(1, page_data.num_pages+1)})
+    context = Context({'post_list' : posts, 'current_page' : int(page), 'total_page': range(1, page_data.num_pages+1)})
 
     return HttpResponse(template.render(context))
+
+def login(request):
+    template = get_template('login_form.html')
+
+    context = Context({'login_form' : LoginForm()})
+    context.update(csrf(request))
+
+    return HttpResponse(template.render(context))
+
+def login_validate(request):
+    # 여기서 로그인 처리!!
+    login_form_data = LoginForm(request.POST)
+    print(login_form_data.data)
+    if login_form_data.is_valid():
+        # 로그인 정보 일치
+        user = auth.authenticate(username=login_form_data.cleaned_data['id'], password=login_form_data.cleaned_data['password'])
+        if user.is_active:
+            auth.login(request, user)
+
+            return redirect('/board')
+        else:
+            return HttpResponse("사용자가 없거나 비밀번호를 잘못 입력하셨습니다.")
+    else:
+        return HttpResponse("로그인 폼이 비정상 입니다.")
+    return HttpResponse("알 수 없는 오류 입니다.")
