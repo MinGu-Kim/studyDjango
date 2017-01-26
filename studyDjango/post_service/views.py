ea9b96e9b747377ae4214e8be7009acc495c3c5f
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-01-26 20:05:06 +0900

    [Django Rest Framework] make Board #2 – Pagination 을 통하여 Pagin 손 쉽게 하기 / Template 안에서 Logic 만들기

--- studyDjango/post_service/views.py
+++ studyDjango/post_service/views.py
@@ -3,6 +3,7 @@ from django.template import Context
 from django.template.loader import get_template
 from django.http.response import HttpResponse
 from post_service.models import Post
+from django.core.paginator import Paginator, PageNotAnInteger, EmptyPage
 
 # Create your views here.
 '''
@@ -11,6 +12,16 @@ Django 에서 View는‘어떤 데이터를 보여줄 것인가?’에 집중
 '''
 def post_list(request):
     template = get_template("post_list.html")
-    context = Context({'post_list' : Post.objects.all()})
+
+    page_data = Paginator(Post.objects.all(), 5)
+    page = request.GET.get('page')
+    try:
+        posts = page_data.page(page)
+    except PageNotAnInteger:
+        posts = page_data.page(1)
+    except EmptyPage:
+        posts = page_data.page(page_data.num_pages)
+
+    context = Context({'post_list' : posts, 'current_page' : page, 'total_page': range(1, page_data.num_pages+1)})
 
     return HttpResponse(template.render(context))
