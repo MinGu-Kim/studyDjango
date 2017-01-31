8f362a94a310b11bdd6871888bca3b826058f7c1
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-01-31 15:57:24 +0900

    [Django Rest Framework] tutorial #4 : Authentication & Permissions @ERROR)invalid literal for int() with base 10: 'owner_id'

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -1,34 +1,20 @@
+from django.contrib.auth.models import User
 from django.forms import widgets
 from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 # ★웹 API를 만들려면 우선 Snippet 클래스의 인스턴스를 json 같은 형태로 직렬화(serializing)하거나 반직렬화(deserializing)할 수 있어야 합니다.
 class SnippetSerializer(serializers.ModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style')
+        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
 
-    # pk = serializers.IntegerField(read_only=True)
-    # title = serializers.CharField(required=False, allow_blank=True, max_length=100)
-    # code = serializers.CharField(style={'base_template': 'textarea.html'})
-    # linenos = serializers.BooleanField(required=False)
-    # language = serializers.ChoiceField(choices=LANGUAGE_CHOICES, default='python')
-    # style = serializers.ChoiceField(choices=STYLE_CHOICES, default='friendly')
-    #
-    # def create(self, validated_data):
-    #     """
-    #     검증한 데이터로 새 `Snippet` 인스턴스를 생성하여 리턴합니다.
-    #     """
-    #     return Snippet.objects.create(**validated_data)
-    #
-    # def update(self, instance, validated_data):
-    #     """
-    #     검증한 데이터로 기존 `Snippet` 인스턴스를 업데이트한 후 리턴합니다.
-    #     """
-    #     instance.title = validated_data.get('title', instance.title)
-    #     instance.code = validated_data.get('code', instance.code)
-    #     instance.linenos = validated_data.get('linenos', instance.linenos)
-    #     instance.language = validated_data.get('language', instance.language)
-    #     instance.style = validated_data.get('style', instance.style)
-    #     instance.save()
-    #     return instance
+
+class UserSerializer(serializers.ModelSerializer):
+    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'snippets')
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -1,21 +1,38 @@
+from django.contrib.auth.models import User
 from django.http import Http404
 from requests import Response
 from rest_framework import generics
 from rest_framework import mixins
+from rest_framework import permissions
 from rest_framework import status
 from rest_framework.views import APIView
 
 from snippets.models import Snippet
-from snippets.serializers import SnippetSerializer
+from snippets.permission import IsOwnerOrReadOnly
+from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework.generics import GenericAPIView
 
 class SnippetList(generics.ListCreateAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
 
 class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = Snippet.objects.all()
     serializer_class = SnippetSerializer
+    permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly,)
+
+class UserList(generics.ListAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
+
+class UserDetail(generics.RetrieveAPIView):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
 
 class snippets_api(GenericAPIView, mixins.ListModelMixin):
     queryset = Snippet.objects.all()
