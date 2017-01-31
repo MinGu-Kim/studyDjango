820fa24eb0c195019f06f2ba5739ab9afe1ad321
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-01-31 14:05:23 +0900

    [Django Rest Framework] tutorial #3 : Class Based Views

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -4,8 +4,8 @@ from snippets import views
 from snippets.views import snippets_api
 
 urlpatterns = [
-    url(r'^snippets/$', views.snippet_list),
-    url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/$', views.SnippetList.as_view()),
+    url(r'^snippets/(?P<pk>[0-9]+)/$', views.SnippetDetail.as_view()),
     url(r'^snippets/api', snippets_api.as_view())
 ]
 
