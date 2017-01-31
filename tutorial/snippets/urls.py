5ba8c9a015a5832c681efc7b267a5c7d7749a4cf
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-01-31 13:57:17 +0900

    [Django Rest Framework] tutorial #2 : Requests and Responses + The Browsable API

--- tutorial/snippets/urls.py
+++ tutorial/snippets/urls.py
@@ -1,7 +1,12 @@
 from django.conf.urls import url
+from rest_framework.urlpatterns import format_suffix_patterns
 from snippets import views
+from snippets.views import snippets_api
 
 urlpatterns = [
     url(r'^snippets/$', views.snippet_list),
     url(r'^snippets/(?P<pk>[0-9]+)/$', views.snippet_detail),
+    url(r'^snippets/api', snippets_api.as_view())
 ]
+
+urlpatterns = format_suffix_patterns(urlpatterns)
