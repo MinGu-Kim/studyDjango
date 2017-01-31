17b2029d568f745be14241f614bf4f6d4754ec68
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-01-31 17:23:33 +0900

    [Django Rest Framework] clear a database : python manage.py flush

--- tutorial/snippets/serializers.py
+++ tutorial/snippets/serializers.py
@@ -4,17 +4,19 @@ from rest_framework import serializers
 from snippets.models import Snippet, LANGUAGE_CHOICES, STYLE_CHOICES
 
 # ★웹 API를 만들려면 우선 Snippet 클래스의 인스턴스를 json 같은 형태로 직렬화(serializing)하거나 반직렬화(deserializing)할 수 있어야 합니다.
-class SnippetSerializer(serializers.ModelSerializer):
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
     owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
 
     class Meta:
         model = Snippet
-        fields = ('id', 'title', 'code', 'linenos', 'language', 'style', 'owner')
+        fields = ('url', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style')
 
 
-class UserSerializer(serializers.ModelSerializer):
-    snippets = serializers.PrimaryKeyRelatedField(many=True, queryset=Snippet.objects.all())
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'snippets')
+        fields = ('url', 'username', 'snippets')
--- tutorial/snippets/views.py
+++ tutorial/snippets/views.py
@@ -4,7 +4,10 @@ from requests import Response
 from rest_framework import generics
 from rest_framework import mixins
 from rest_framework import permissions
+from rest_framework import renderers
 from rest_framework import status
+from rest_framework.decorators import api_view
+from rest_framework.reverse import reverse
 from rest_framework.views import APIView
 
 from snippets.models import Snippet
@@ -34,9 +37,17 @@ class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
 
-class snippets_api(GenericAPIView, mixins.ListModelMixin):
+@api_view(('GET',))
+def api_root(request, format=None):
+    return Response({
+        'users': reverse('user-list', request=request, format=format),
+        'snippets': reverse('snippet-list', request=request, format=format)
+    })
+
+class SnippetHighlight(generics.GenericAPIView):
     queryset = Snippet.objects.all()
-    serializer_class = SnippetSerializer
+    renderer_classes = (renderers.StaticHTMLRenderer,)
 
     def get(self, request, *args, **kwargs):
-        return self.list(request, *args, **kwargs)
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
