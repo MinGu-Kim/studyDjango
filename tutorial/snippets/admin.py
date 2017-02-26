c57704ddeaf0b75557996579d514f93efec38183
DESKTOP-SPINMKP\KimMinGoo <kimmingoo73@gmail.com>
2017-02-27 00:55:41 +0900

    add snippet admin

--- tutorial/snippets/admin.py
+++ tutorial/snippets/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from snippets.models import Snippet
+
+
+class SnippetAdmin(admin.ModelAdmin):
+    list_display = ('owner', 'title', 'code', 'linenos', 'language', 'style')
+
+admin.site.register(Snippet, SnippetAdmin)
